3523bce083e6ba46c29402bc01cb244a7aee2504
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-13 03:04:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do {|student_hash| self.new 
+    attributes_hash.each do {|student_hash| self.new(student)
   end
 
   def self.all

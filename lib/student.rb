f5ddb4634ca05d86355eeb0c60ef416256406abb
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-13 03:02:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ class Student
   def initialize(student_hash)
     attributes.each_pair { |key, value| self.send(("#{key}="), value) }
     @@all << self
-end
+  end
 
   def self.create_from_collection(students_array)
     

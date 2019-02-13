4d88988a885a1aa5cc627c78433307b88b3b2bad
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-13 02:56:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,17 @@
 require 'open-uri'
 require 'pry'
 
-class Scraper
-
   def self.scrape_index_page(index_url)
-  
-  end
 
-  def self.scrape_profile_page(profile_url)
-    
-  end
-
-end
-
+     students_hash = []
+    html = Nokogiri::HTML(open(index_url))
+    html.css(".student-card").collect do |student|
+      hash = {
+        name: student.css("h4.student-name").text,
+        location: student.css("p.student-location").text,
+        profile_url: "http://students.learn.co/" + student.css("a").attribute("href")
+      }
+      students_hash << hash
+    end
+    students_hash
+  end	  end
